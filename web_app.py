7e1d96fa6cf24bacc3e9b50c22ba6199403d889b
Ashwath <74086160+AchuAshwath@users.noreply.github.com>
2022-05-06 15:56:36 +0530

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -1,5 +1,23 @@
 from PIL import Image
 import streamlit as st
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing import image
+
+model= tf.keras.models.load_model('bestmodel.h5') # Loading the Tensorflow Saved Model
+
+def get_img_array(img_path):
+  """
+  Input : Takes in image path as input 
+  Output : Gives out Pre-Processed image
+  """
+  path = img_path
+  img = image.load_img(path, target_size=(224,224,3))
+  img = image.img_to_array(img)
+  img = np.expand_dims(img , axis= 0 )
+  
+  return img
 
 st.write("""
 # Covid-19 Test
@@ -16,9 +34,4 @@ upload_file = st.sidebar.file_uploader("Upload your jpeg file here", type=["jpeg
 
 st.write("""***""")
 
-
-image = Image.open("D:/achu/programs/miniProject/kjr-21-e25-g001-l-a.jpg")
-st.image(image, use_column_width=True)
-
-print(type(image))
 
