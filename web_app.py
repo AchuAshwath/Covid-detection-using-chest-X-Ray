8ac37fe9a71995fa77ba46ac859995eda91b31b7
Ashwath <74086160+AchuAshwath@users.noreply.github.com>
2022-07-07 10:44:09 +0530

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -1,27 +1,21 @@
+from asyncore import write
 from PIL import Image
 import streamlit as st
 from tensorflow.keras.preprocessing import image
 import tensorflow as tf
 import numpy as np
 from keras.preprocessing import image
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+#from prediction import predict
+import os
 
 model= tf.keras.models.load_model('bestmodel.h5') # Loading the Tensorflow Saved Model
-
-def get_img_array(img_path):
-  """
-  Input : Takes in image path as input 
-  Output : Gives out Pre-Processed image
-  """
-  path = img_path
-  img = image.load_img(path, target_size=(224,224,3))
-  img = image.img_to_array(img)
-  img = np.expand_dims(img , axis= 0 )
-  
-  return img
+class_type = {0:'Covid',  1 : 'Normal'}
 
 st.write("""
 # Covid-19 Test
- This app predicts if the given x-ray is covid positive or not!
+ This App Predicts if the given X-ray is Covid Positive or Not!
 """)
 
 # creating the sidebar
@@ -30,8 +24,27 @@ st.sidebar.markdown("""
 [Example Jpeg input file](https://github.com/AchuAshwath/MiniProject/blob/main/IM-0143-0001.jpeg)
 """)
 
-upload_file = st.sidebar.file_uploader("Upload your jpeg file here", type=["jpeg", "jpg"])
-
+upload_file = st.sidebar.file_uploader("Upload your jpeg file here", type=["jpeg", "jpg","png"])
 st.write("""***""")
 
+if upload_file is not None:
+  image = Image.open(upload_file)
+  st.image(image, caption='Uploaded Image.', use_column_width=True)
+  img = Image.open(upload_file)
+  img.save("temp_photo.jpg")
+  path = "temp_photo.jpg"
+  img = load_img(path, target_size=(224,224,3))
+  img = img_to_array(img)
+  img = np.expand_dims(img , axis= 0 )
+  res = class_type[np.argmax(model.predict(img))]
+  st.write("""The given X-Ray image is of type = """,res)
+else:
+  #ipo theeku nothing
+  st.write("""No Image Uploaded""")
+  
 
+
+
+#st.write("""The chances of image being Covid is : """,model.predict(image)[0][0]*100,""" percent""")
+
+#st.write("""The chances of image being Normal is : """,model.predict(image)[0][1]*100,""" percent""")
